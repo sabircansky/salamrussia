af2b5f815feebfbc5e4658ff1337ffcce4308c82
sabircansky <63296990+sabircansky@users.noreply.github.com>
2024-11-07 16:18:27 +0300

    Add files via upload

--- handlers/language_selection.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-from utils.language_codes import LANGUAGE_CODES
-
-async def handle_language_selection(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    language_buttons = [[lang] for lang in LANGUAGE_CODES.keys()]
-    await update.message.reply_text("Выберите язык:", reply_markup={"keyboard": language_buttons})
